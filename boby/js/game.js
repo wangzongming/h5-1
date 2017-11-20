3f64fe00fc13104af6019fd6647bcacce4edf66f
wang <wang_z_m@163.com>
2017-11-21 02:37:44 +0800

    2.30了

--- boby/js/game.js
+++ boby/js/game.js
@@ -9,13 +9,12 @@ var wxx = {
     scoreEl:'.score-num',//得分容器
     goodTextEl:'#good',
     score:0,//得分
-    gameTime:10,//游戏时间 s  写数字
+    gameTime:50,//游戏时间 s  写数字
+    randomSweetInt:500,//出糖果的间隔
     upIng:false,//是否正在跳跃
-    sweetTimer:'',//每个精灵的计时器 随机变量名
-    touchStartY:'',//用于计算跳跃
     goodText:true,//是否开点赞
     musicBtn: true,//是否开启音乐按钮
-    musicIsAutoPlay: true,//音乐是否自动播放 
+    musicIsAutoPlay: false,//音乐是否自动播放 
     musicUrl: 'http://sc1.111ttt.com/2017/1/11/11/304112002347.mp3',//音乐地址
     sweetData:[{//糖果类型 随机出
         type:'good',
@@ -58,7 +57,7 @@ var wxx = {
           audio.play();
           wxx.musicIsAutoPlay = true;
         }
-      },
+    },
     crash:function(domOne, domTwo, cb){//碰撞检测  回调
         var $domOne = $(domOne);
         var $domTwo = $(domTwo);
@@ -72,21 +71,19 @@ var wxx = {
         var domOneWidth =  $domOne.width(); //角色的宽
         var domTwoWidth =  $domTwo.width(); //糖果的宽
 
-        // console.log(wxx.upIng);
         if(wxx.upIng){//正在跳跃
             cb('false');
             return;
-        }
+        }   
         if(domTwoX > domOneX - domTwoWidth  && domTwoX <    domOneX +  domOneWidth   ){//碰撞到糖果  左右距离检测
             if( domTwoY >= domOneY - 10 && domTwoY < domOneY + 100 ){//上下距离检测
                 cb($domTwo);//执行回调
-            }else{
+            }else{  
                 cb('false');
             }
         }else{
             cb('false');
         }
-        
     },
     playGame:function(cb){//开始游戏 点击后触发  回调
         $(wxx.indexPageEl).fadeOut('slow',function(){
@@ -107,7 +104,7 @@ var wxx = {
             console.log("请求错误")
           }
         })
-      },
+    },
     gameOver:function(gameTimer, html, title,cb){ //定时器必传  默认不管即可
         var title = title || 'GAME OVER';
         $(wxx.timeEl).html( '0' );
@@ -134,62 +131,107 @@ var wxx = {
                 //some code ....
         return html;
     },
+    createSweet:function(opt,cb){//创建糖果  回调
+        var className = opt.className,
+                type = opt.type,
+                result = opt.result,
+                html = opt.html,
+                key = opt.key,
+                dom = opt.insertDom,
+                left = opt.style.left;
+
+        var $div = $('<div>',{
+            'class':className,
+            'type':type,
+            'result':result,
+            'key':key,
+            'html':html,
+        }).appendTo(dom);
+        $div.css({
+            left:left
+        })
+        cb($div);
+    },
+    className:'',//---------------------这些都是必备的临时变量
+    keyframes:10,//多少帧
+    sweetRanVar:0,//临时变量
+    sweetCount:[],//页面的糖果 用于检测碰撞
+    testIndex:'',//定时器
+    sweetTimer:'',//每个精灵的计时器 随机变量名
+    touchStartY:'',//用于计算跳跃
+
     playGameFn:function(){//游戏逻辑
         var gameTimer = setInterval(function(){//创建糖果
-            var random = parseInt( Math.random() * 10000 + new Date().getTime()  );//class用
-            wxx.gameTime --;
-            var timer = wxx.gameTime;//倒计时
-            if( timer<0 ){//结束游戏
-                wxx.gameOver(gameTimer,  wxx.gameOverHtml(wxx.score),  'GAME OVER', function(){
-                    window.location.href =  window.location.href;
-                }) 
-            }else{
-                $(wxx.timeEl).text( timer );
-            }
-    
-            var sweetRandom = parseInt( Math.random() * (wxx.sweetData.length) );//随机糖果类型
-            var sweetLeftRandom = parseInt(  Math.random() * (wxx.winWidth - $( wxx.sweetEl ).width() )  );
-            console.log(sweetLeftRandom);
-            var className = random.toString();
-            var $div = $('<div>',{
-                'class':'sweet ' + className,
-                'type':wxx.sweetData[sweetRandom].type,
-                'result':wxx.sweetData[sweetRandom].result,
-                'html':wxx.sweetData[sweetRandom].img,
-                'click':function(){
-                    // console.log($(this))
+            wxx.sweetRanVar += 10;
+
+            if(wxx.sweetRanVar >= wxx.randomSweetInt){//在设置里的事件随机出糖果
+                var sweetIndex = wxx.sweetCount.length - 1;
+                var random = parseInt( Math.random() * 10000 + new Date().getTime()  );//class用
+                wxx.gameTime -- ;
+                var timer = wxx.gameTime;//倒计时
+                if( timer<0 ){//结束游戏
+                    wxx.gameOver(gameTimer,  wxx.gameOverHtml(wxx.score),  'GAME OVER', function(){
+                        window.location.href =  window.location.href;
+                    }) 
+                }else{
+                    $(wxx.timeEl).text( timer );
                 }
-            }).appendTo(wxx.gamePageEl);
-            $div.css({
-                left:sweetLeftRandom
-            })
-    
-            wxx.sweetTimer.className = setInterval(function(){
-                wxx.crash(wxx.protagonistEl, '.'+className, function(sweet){
+                var sweetRandom = parseInt( Math.random() * (wxx.sweetData.length-1) );//随机糖果类型
+                var sweetLeftRandom = parseInt(  Math.random() * (wxx.winWidth - $( wxx.sweetEl ).width() )  );
+                wxx.className = random.toString();
+                wxx.createSweet({//创建糖果
+                    className:'sweet ' +  wxx.className,
+                    key:sweetIndex,
+                    type:wxx.sweetData[sweetRandom].type,
+                    result:wxx.sweetData[sweetRandom].result.toString(),
+                    style:{
+                        left:sweetLeftRandom
+                    },
+                    html:wxx.sweetData[sweetRandom].img,
+                    insertDom:wxx.gamePageEl
+                },function($this){
+                   wxx.sweetCount.push(wxx.className);
+                   wxx.testIndex.random = setTimeout(function(){
+                        wxx.sweetCount.splice(sweetIndex, 1);
+                        // $('.'+wxx.className).remove();  //浪费内存 
+                        clearTimeout( wxx.testIndex.random  )
+                    },4000)
+                })
+                wxx.sweetRanVar  = 0;            
+            }
+
+            $.each( wxx.sweetCount, function(i, v){
+                wxx.crash(wxx.protagonistEl, '.' + v.toString(), function(sweet){
+                    // console.log(sweet);
                     if(sweet == 'false'){//清除定时器即可
                         clearInterval(wxx.sweetTimer.className);
                         return;
                     }
+                    wxx.className = '';//清除class
 
-                    if(wxx.goodText){
-                        $(wxx.goodTextEl).fadeIn('10',function(){//点赞按钮
-                            $(wxx.goodTextEl).fadeOut('slow');
-                        })
-                    }
-                   
                     var result = $(sweet).attr('result');
-                    if(wxx.score >= 0){
-                        wxx.score = wxx.score + Number(result) ;
-                        $(wxx.scoreEl).html(wxx.score);
+                    if(Number(result)  > 0){
+                        if(wxx.goodText){
+                            $(wxx.goodTextEl).fadeIn('10',function(){//点赞按钮
+                                $(wxx.goodTextEl).fadeOut('slow');
+                            })
+                        }
+                    }
+                    if( Number(wxx.score) >= 0){     
+                        // console.log('执行加分', result)
+                        wxx.score = Number(wxx.score) + Number(result) ;
+                        // console.log(wxx.score)
+                        $(wxx.scoreEl).html( wxx.score );
                     }else{
                         wxx.score = 0;
-                        $(wxx.scoreEl).html(0);
+                        // $(wxx.scoreEl).html(0);
                     }
                     clearInterval(wxx.sweetTimer.className);
                     $(sweet).hide('slow');
+                    // $(sweet).remove()
                 });
-            },400)
-        },1000);
+            })
+        },10);//10毫秒执行一次轮训
     },
     createRankingDom: function (data, cb) {
         var html = '<ul class="ranking-ul">';
@@ -224,7 +266,6 @@ var wxx = {
     var isautoplay = "autoplay";
     wxx.musicIsAutoPlay ? isautoplay = "autoplay" : isautoplay = '';
     if (wxx.musicBtn === true) {
-        console.log(12)
         var $music = $('<div id="music" class="' + (wxx.musicIsAutoPlay ? 'musicBtn' : '') + '" >  <audio preload="load" id="musicMP3"  ' + isautoplay + '  src=" ' + wxx.musicUrl + ' "></audio></div>');
             $music.click(function () {//音乐"#musicMP3"
             wxx.music(wxx.musicEl, $(this));
@@ -254,7 +295,7 @@ $('body').css({
     height:$(window).height()
 })
 
-$('body').on('click',function(event){//页面的的一些点击事件
+$('body').on('click',function(event){//页面的的一些用户事件
     var evType = event.type;
     var target = $(event.target).attr('class');
     switch(evType){
@@ -313,13 +354,13 @@ $('#game').on('touchstart touchmove touchend', function(event){
 
             if(touch.pageX > wxx.winWidth / 2){//往右移动
                 $(wxx.protagonistEl).css({
-                    left:'50%',
+                    left:'40%',
                     right:'0rem'
                 })
             }else if(touch.pageX < wxx.winWidth / 2){//往左走
                 $(wxx.protagonistEl).css({
                     left:'0rem',
-                    right:'50%'
+                    right:'40%'
                 })
             }
             
